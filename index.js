08e033e87a3d3c2b44f5abd8ec9ea604559e6452
BuildTools <unconfigured@null.spigotmc.org>
2020-08-26 15:44:45 -0300

    belugacdn is down, using braintv instead

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const addon = new addonBuilder(manifest);
 addon.defineStreamHandler(async (args) => {
     let [imdbId, season, episode] = args.id.split(":");
     let { id, name } = await getMeta(imdbId);
-    let token = await getToken();
+    //let token = await getToken();
 
     let streams = [
         {
             id: args.id,
             title: name,
             type: `series`,
-            url: `https://${token}.belugacdn.link/${id}-${season}-${episode}.mp4`,
+            url: `https://braintv.azureedge.net/tv/${id}/${season}/dub/${episode}.mp4`//`https://${token}.belugacdn.link/${id}-${season}-${episode}.mp4`,
         },
     ];
 
